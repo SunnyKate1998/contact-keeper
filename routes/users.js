d77a9b45e5a4798325227f88eba6677e50cb445d
Kate Fang <fm980513@126.com>
2022-01-17 14:14:53 -0600

    Basic settings and connect to the database.(MongoDB Atlas)

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   POST api/users
+// @desc    Register a user
+// @access  Public
+router.post('/', (req, res) => {
+    // Slash now pertains to api/users
+    res.send('Register a user');
+});
+
+module.exports = router;
