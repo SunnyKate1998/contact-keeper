5ffdd69c497b46bd3483fe21a8993a322c4b262d
Kate Fang <fm980513@126.com>
2022-01-18 12:05:13 -0600

    Add User model and implement register route. Using mongoose and JWT.

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,78 @@
+const bcrypt = require('bcryptjs');
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator/check');
+
+const User = require('../models/User');
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req, res) => {
-    // Slash now pertains to api/users
-    res.send('Register a user');
-});
+router.post(
+    '/', // Slash now pertains to api/users
+    // Validation for message in the request body
+    [
+        check('name', 'Please add name').not().isEmpty(),
+        check('email', 'Please include a valid email').isEmail(),
+        check(
+            'password',
+            'Please enter a password with 6 or more characters'
+        ).isLength({ min: 6 }),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { name, email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+
+            if (user) {
+                return res.status(400).json({ msg: 'User already exists.' });
+            }
+
+            user = new User({
+                name,
+                email,
+                password,
+            });
+
+            // Encrypt user's password
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+
+            // Save the new user in the database
+            user.save();
+
+            // Data want to be sent to jwt payload
+            const payload = {
+                user: {
+                    id: user.id,
+                },
+            };
+
+            // Generate token
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                {
+                    expiresIn: 360000,
+                },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send('Server Error');
+        }
+    }
+);
 
 module.exports = router;
