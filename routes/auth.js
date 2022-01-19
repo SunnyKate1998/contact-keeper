140e1d0fe0afbb90462bfb744c7a9877daf296a8
Kate Fang <fm980513@126.com>
2022-01-19 05:58:03 -0600

    Implemented authenticate route and add auth middleware and alse implemented protected route(get logged user).

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,83 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator/check');
+const User = require('../models/User');
 
 // @route   GET api/auth
 // @desc    Get logged in user
-// @access  Private
-router.get('/', (req, res) => {
-    res.send('Get logged in user');
+// @access  Private (Need to log in)
+router.get('/', auth, async (req, res) => {
+    // If logged in, req should have a user object (assigned by auth middleware)
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 // @route   POST api/auth
 // @desc    Auth user & get token
 // @access  Public
-router.post('/', (req, res) => {
-    res.send('Log in user');
-});
+router.post(
+    '/',
+    [
+        check('email', 'Please include a valid email').isEmail(),
+        check('password', 'Password is required').exists(),
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
+
+        const { email, password } = req.body;
+
+        try {
+            let user = await User.findOne({ email });
+
+            // The email have not been registered.
+            if (!user) {
+                return res.status(404).json({ msg: 'Invalid email.' });
+            }
+
+            // Check password input.
+            const isMatch = await bcrypt.compare(password, user.password);
+
+            // The user exist but password input is wrong.
+            if (!isMatch) {
+                return res.status(400).json({ msg: 'Incorrect password.' });
+            }
+
+            // User is validated. Then can be sent a jwt.
+            const payload = {
+                user: {
+                    id: user.id,
+                },
+            };
+
+            // Generate token
+            jwt.sign(
+                payload,
+                config.get('jwtSecret'),
+                {
+                    expiresIn: 360000,
+                },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                }
+            );
+        } catch (error) {
+            console.error(error.message);
+            res.status(500).send('Server Error');
+        }
+    }
+);
 
 module.exports = router;
