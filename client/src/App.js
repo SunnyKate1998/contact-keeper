ef7eace01ba15d57ec56d9a2743bc6c12da8ecb7
Kate Fang <fm980513@126.com>
2022-02-13 15:53:46 -0600

    Developed everything about alert.

--- client/src/App.js
+++ client/src/App.js
@@ -5,39 +5,44 @@ import Home from './components/pages/Home';
 import About from './components/pages/About';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
+import Alerts from './components/layouts/Alerts';
 import ContactState from './context/contact/contactState';
 import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
     return (
         <AuthState>
             <ContactState>
-                <Router>
-                    <Fragment>
-                        <Navbar />
-                        <div className="container">
-                            <Routes>
-                                <Route exact path="/" element={<Home />} />
-                                <Route
-                                    exact
-                                    path="/about"
-                                    element={<About />}
-                                />
-                                <Route
-                                    exact
-                                    path="/register"
-                                    element={<Register />}
-                                />
-                                <Route
-                                    exact
-                                    path="/login"
-                                    element={<Login />}
-                                />
-                            </Routes>
-                        </div>
-                    </Fragment>
-                </Router>
+                <AlertState>
+                    <Router>
+                        <Fragment>
+                            <Navbar />
+                            <div className="container">
+                                <Alerts />
+                                <Routes>
+                                    <Route exact path="/" element={<Home />} />
+                                    <Route
+                                        exact
+                                        path="/about"
+                                        element={<About />}
+                                    />
+                                    <Route
+                                        exact
+                                        path="/register"
+                                        element={<Register />}
+                                    />
+                                    <Route
+                                        exact
+                                        path="/login"
+                                        element={<Login />}
+                                    />
+                                </Routes>
+                            </div>
+                        </Fragment>
+                    </Router>
+                </AlertState>
             </ContactState>
         </AuthState>
     );
